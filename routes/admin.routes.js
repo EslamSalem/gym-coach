912ac7db53f8a7278b5d427965f7defae025bf15
Eslam Salem <eslamsalem2.es@gmail.com>
2023-11-16 12:56:45 +0200

    added manage logs/nutrition admin pages

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -6,4 +6,12 @@ router.get("/users", function(req, res) {  // /admin/users
   res.render("admin/manage-users");
 });
 
+router.get("/logs", function(req, res) {  // /admin/logs
+  res.render("admin/manage-logs");
+});
+
+router.get("/nutrition", function(req, res) {  // /admin/nutrition
+  res.render("admin/manage-nutrition");
+});
+
 module.exports = router;
